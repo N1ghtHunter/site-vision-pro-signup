1a0bcaf7bf6eccf5af9aaf5f155e6d47160c1de1
Mazin Islam <mazinislam431@gmail.com>
2024-10-07 00:15:54 +0300

    Refactor main.jsx to add MUI theme and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,18 @@ import './index.css';
 import SignupForm from './pages/SignupForm';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+const theme = createTheme({
+	typography: {
+		fontFamily: 'Roboto Flex, sans-serif',
+	},
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<ToastContainer />
-		<SignupForm />
+		<ThemeProvider theme={theme}>
+			<ToastContainer />
+			<CssBaseline />
+			<SignupForm />
+		</ThemeProvider>
 	</React.StrictMode>
 );
